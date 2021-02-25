f6f80802e249073a328d0ceb412104ef8d045096
kanyawee2540 <kanyawi.p@gmail.com>
2021-02-25 23:44:40 +0700

    update

---
+++ Assets/PlayerScores.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FullSerializer;
+using Proyecto26;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class PlayerScores : MonoBehaviour
+{
+    /*public Text scoreText;
+    public InputField getScoreText;
+
+    public InputField firstNameText;
+    public InputField lastNameText;
+    public InputField emailText;
+    public InputField usernameText;
+    public InputField passwordText;
+    public InputField confirmPasswordText;*/
+
+    //Login variables
+    [Header("Login")]
+    public InputField emailLoginField;
+    public InputField passwordLoginField;
+    //public Text warningLoginText;
+    //public Text confirmLoginText;
+
+    //Register variables
+    [Header("Register")]
+    public InputField nameRegisterField;
+    public InputField surnameRegisterField;
+    public InputField emailRegisterField;
+    public InputField usernameRegisterField;
+    public InputField passwordRegisterField;
+    public InputField passwordRegisterVerifyField;
+    //public Text warningRegisterText;
+
+
+    private System.Random random = new System.Random(); 
+
+    User user = new User();
+
+    private string databaseURL = "https://project-75a5c-default-rtdb.firebaseio.com/users"; 
+    private string AuthKey = "AIzaSyBdvdw8w_v66y96r7ndXpKRs39lMiZwABY";
+    
+    public static fsSerializer serializer = new fsSerializer();
+    
+    
+    public static int playerScore;
+    public static string playerName;
+
+    private string idToken;
+    
+   public static string localId;
+
+    // private string getLocalId;
+    
+    /*
+    private void Start()
+    {
+        playerScore = random.Next(0, 101);
+        scoreText.text = "Score: " + playerScore;
+    }
+
+    public void OnSubmit()
+    {
+        PostToDatabase();
+    }
+    
+    public void OnGetScore()
+    {
+        GetLocalId();
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = "Score: " + user.userScore;
+    }
+    */
+
+    private void PostToDatabase(bool emptyScore = false, string idTokenTemp = "")
+    {
+        if (idTokenTemp == "")
+        {
+            idTokenTemp = idToken;
+        }
+        
+        User user = new User();
+        
+        RestClient.Put(databaseURL + "/" + localId + ".json?auth=" + idTokenTemp, user);
+    }
+
+    /*
+    private void RetrieveFromDatabase()
+    {
+        RestClient.Get<User>(databaseURL + "/" + getLocalId + ".json?auth=" + idToken).Then(response =>
+            {
+                user = response;
+                UpdateScore();
+            });
+    }
+    */
+
+    public void SignUpUserButton()
+    {
+        SignUpUser(emailRegisterField.text, usernameRegisterField.text, passwordRegisterField.text);
+    }
+    
+  /*  public void SignInUserButton()
+    {
+        SignInUser(emailText.text, passwordText.text);
+    }
+    
+    */
+    private void SignUpUser(string email, string username, string password)
+    {
+        string userData = "{\"email\":\"" + email + "\",\"password\":\"" + password + "\",\"returnSecureToken\":true}";
+        RestClient.Post<SignResponse>("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=" + AuthKey, userData).Then(
+            response =>
+            {
+                string emailVerification = "{\"requestType\":\"VERIFY_EMAIL\",\"idToken\":\"" + response.idToken + "\"}";
+                RestClient.Post(
+                    "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getOobConfirmationCode?key=" + AuthKey,
+                    emailVerification);
+                localId = response.localId;
+                playerName = username;
+                PostToDatabase(true, response.idToken);
+                
+            }).Catch(error =>
+        {
+            Debug.Log(error);
+        });
+    }
+    
+   /* private void SignInUser(string email, string password)
+    {
+        string userData = "{\"email\":\"" + email + "\",\"password\":\"" + password + "\",\"returnSecureToken\":true}";
+        RestClient.Post<SignResponse>("https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" + AuthKey, userData).Then(
+            response =>
+            {
+                string emailVerification = "{\"idToken\":\"" + response.idToken + "\"}";
+                RestClient.Post(
+                    "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getAccountInfo?key=" + AuthKey,
+                    emailVerification).Then(
+                    emailResponse =>
+                    {
+
+                        fsData emailVerificationData = fsJsonParser.Parse(emailResponse.Text);
+                        EmailConfirmationInfo emailConfirmationInfo = new EmailConfirmationInfo();
+                        serializer.TryDeserialize(emailVerificationData, ref emailConfirmationInfo).AssertSuccessWithoutWarnings();
+                        
+                        if (emailConfirmationInfo.users[0].emailVerified)
+                        {
+                            idToken = response.idToken;
+                            localId = response.localId;
+                            GetUsername();
+                        }
+                        else
+                        {
+                            Debug.Log("You are stupid, you need to verify your email dumb");
+                        }
+                    });
+                
+            }).Catch(error =>
+        {
+            Debug.Log(error);
+        });
+    }*/
+
+    /*
+    private void GetUsername()
+    {
+        RestClient.Get<User>(databaseURL + "/" + localId + ".json?auth=" + idToken).Then(response =>
+        {
+            playerName = response.userName;
+        });
+    }*/
+    
+    /*
+    private void GetLocalId(){
+        RestClient.Get(databaseURL + ".json?auth=" + idToken).Then(response =>
+        {
+            var username = getScoreText.text;
+            
+            fsData userData = fsJsonParser.Parse(response.Text);
+            Dictionary<string, User> users = null;
+            serializer.TryDeserialize(userData, ref users);
+
+            foreach (var user in users.Values)
+            {
+                if (user.userName == username)
+                {
+                    getLocalId = user.localId;
+                    RetrieveFromDatabase();
+                    break;
+                }
+            }
+        }).Catch(error =>
+        {
+            Debug.Log(error);
+        });
+    }*/
+}
--- Assets/RestClient/DemoScene/Scripts/MainScript.cs
+++ Assets/RestClient/DemoScene/Scripts/MainScript.cs
@@ -3,106 +3,73 @@ using UnityEditor;
 using Models;
 using Proyecto26;
 using System.Collections.Generic;
-using UnityEngine.Networking;
 
 public class MainScript : MonoBehaviour {
 
-	private readonly string basePath = "https://jsonplaceholder.typicode.com";
+    private readonly string basePath = "https://jsonplaceholder.typicode.com";
 	private RequestHelper currentRequest;
-
-	private void LogMessage(string title, string message) {
-#if UNITY_EDITOR
-		EditorUtility.DisplayDialog (title, message, "Ok");
-#else
-		Debug.Log(message);
-#endif
-	}
 
 	public void Get(){
 
 		// We can add default request headers for all requests
 		RestClient.DefaultRequestHeaders["Authorization"] = "Bearer ...";
 
-		RequestHelper requestOptions = null;
+        RequestHelper requestOptions = null;
 
 		RestClient.GetArray<Post>(basePath + "/posts").Then(res => {
-			this.LogMessage("Posts", JsonHelper.ArrayToJsonString<Post>(res, true));
-			return RestClient.GetArray<Todo>(basePath + "/todos");
+            EditorUtility.DisplayDialog ("Posts", JsonHelper.ArrayToJsonString<Post>(res, true), "Ok");
+            return RestClient.GetArray<Todo>(basePath + "/todos");
 		}).Then(res => {
-			this.LogMessage("Todos", JsonHelper.ArrayToJsonString<Todo>(res, true));
-			return RestClient.GetArray<User>(basePath + "/users");
+            EditorUtility.DisplayDialog ("Todos", JsonHelper.ArrayToJsonString<Todo>(res, true), "Ok");
+            return RestClient.GetArray<User>(basePath + "/users");
 		}).Then(res => {
-			this.LogMessage("Users", JsonHelper.ArrayToJsonString<User>(res, true));
+			EditorUtility.DisplayDialog ("Users", JsonHelper.ArrayToJsonString<User>(res, true), "Ok");
+
 
 			// We can add specific options and override default headers for a request
 			requestOptions = new RequestHelper { 
 				Uri = basePath + "/photos",
 				Headers = new Dictionary<string, string> {
 					{ "Authorization", "Other token..." }
-				},
-				EnableDebug = true
+				}
 			};
 			return RestClient.GetArray<Photo>(requestOptions);
 		}).Then(res => {
-			this.LogMessage("Header", requestOptions.GetHeader("Authorization"));
+			EditorUtility.DisplayDialog("Header", requestOptions.GetHeader("Authorization"), "Ok");
 
-			// And later we can clear the default headers for all requests
-			RestClient.ClearDefaultHeaders();
+			// And later we can clean the default headers for all requests
+			RestClient.CleanDefaultHeaders();
 
-		}).Catch(err => this.LogMessage("Error", err.Message));
+		}).Catch(err => EditorUtility.DisplayDialog ("Error", err.Message, "Ok"));
 	}
 
 	public void Post(){
 
-		// We can add default query string params for all requests
-		RestClient.DefaultRequestParams["param1"] = "My first param";
-		RestClient.DefaultRequestParams["param3"] = "My other param";
-
 		currentRequest = new RequestHelper {
 			Uri = basePath + "/posts",
-			Params = new Dictionary<string, string> {
-				{ "param1", "value 1" },
-				{ "param2", "value 2" }
-			},
 			Body = new Post {
 				title = "foo",
 				body = "bar",
 				userId = 1
-			},
-			EnableDebug = true
+			}
 		};
 		RestClient.Post<Post>(currentRequest)
-		.Then(res => {
-
-			// And later we can clear the default query string params for all requests
-			RestClient.ClearDefaultParams();
-
-			this.LogMessage("Success", JsonUtility.ToJson(res, true));
-		})
-		.Catch(err => this.LogMessage("Error", err.Message));
+		.Then(res => EditorUtility.DisplayDialog ("Success", JsonUtility.ToJson(res, true), "Ok"))
+		.Catch(err => EditorUtility.DisplayDialog ("Error", err.Message, "Ok"));
 	}
 
 	public void Put(){
 
-		currentRequest = new RequestHelper {
-			Uri = basePath + "/posts/1", 
-			Body = new Post {
-				title = "foo",
-				body = "bar",
-				userId = 1
-			},
-			Retries = 5,
-			RetrySecondsDelay = 1,
-			RetryCallback = (err, retries) => {
-				Debug.Log (string.Format ("Retry #{0} Status {1}\nError: {2}", retries, err.StatusCode, err));
+		RestClient.Put<Post>(basePath + "/posts/1", new Post {
+			title = "foo",
+			body = "bar",
+			userId = 1
+		}, (err, res, body) => {
+			if(err != null){
+				EditorUtility.DisplayDialog ("Error", err.Message, "Ok");
 			}
-		};
-		RestClient.Put<Post>(currentRequest, (err, res, body) => {
-			if (err != null){
-				this.LogMessage("Error", err.Message);
-			}
-			else {
-				this.LogMessage("Success", JsonUtility.ToJson(body, true));
+			else{
+				EditorUtility.DisplayDialog ("Success", JsonUtility.ToJson(body, true), "Ok");
 			}
 		});
 	}
@@ -110,11 +77,11 @@ public class MainScript : MonoBehaviour {
 	public void Delete(){
 
 		RestClient.Delete(basePath + "/posts/1", (err, res) => {
-			if (err != null){
-				this.LogMessage("Error", err.Message);
+			if(err != null){
+				EditorUtility.DisplayDialog ("Error", err.Message, "Ok");
 			}
-			else {
-				this.LogMessage("Success", "Status: " + res.StatusCode.ToString());
+			else{
+				EditorUtility.DisplayDialog ("Success", "Status: " + res.StatusCode.ToString(), "Ok");
 			}
 		});
 	}
@@ -124,22 +91,5 @@ public class MainScript : MonoBehaviour {
 			currentRequest.Abort();
 			currentRequest = null;
 		}
-	}
-
-	public void DownloadFile(){
-
-		var fileUrl = "https://raw.githubusercontent.com/IonDen/ion.sound/master/sounds/bell_ring.ogg";
-		var fileType = AudioType.OGGVORBIS;
-
-		RestClient.Get(new RequestHelper {
-			Uri = fileUrl,
-			DownloadHandler = new DownloadHandlerAudioClip(fileUrl, fileType)
-		}).Then(res => {
-			AudioSource audio = GetComponent<AudioSource>();
-			audio.clip = ((DownloadHandlerAudioClip)res.Request.downloadHandler).audioClip;
-			audio.Play();
-		}).Catch(err => {
-			this.LogMessage("Error", err.Message);
-		});
 	}
 }
